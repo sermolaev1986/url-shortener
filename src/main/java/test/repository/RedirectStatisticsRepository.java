3fb5090ce1a9668833400dcb7080d00e296b3d32
Sergey Ermolaev <Sergey_Ermolaev1@epam.com>
2017-07-25 12:48:54 +0300

    - fixed missing @Modifying annotation

--- src/main/java/test/repository/RedirectStatisticsRepository.java
+++ src/main/java/test/repository/RedirectStatisticsRepository.java
@@ -1,5 +1,6 @@
 package test.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +13,7 @@ import java.util.List;
 public interface RedirectStatisticsRepository extends Repository<RedirectStatistics, String> {
 
     @Query("UPDATE RedirectStatistics rs SET rs.redirectCount = rs.redirectCount + 1 WHERE rs.fullUrl = :fullUrl")
+    @Modifying
     void incrementRedirectCount(@Param("fullUrl") String fullUrl);
 
     @Query("SELECT rs.id FROM RedirectStatistics rs WHERE rs.urlMapping.account = :accountId")
